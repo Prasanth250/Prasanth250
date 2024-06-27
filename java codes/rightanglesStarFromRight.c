00e9e574a8c015edbe0337beb91e3f4e62441388
Prasanth250 <147532770+Prasanth250@users.noreply.github.com>
2024-06-27 14:24:27 +0530

    Create rightanglesStarFromRight.c

---
+++ java codes/rightanglesStarFromRight.c
@@ -0,0 +1,14 @@
+import java.io.*;
+class rsn {
+    public static void main(String[] args) {
+        int row,star;
+        for(row=1;row<=10;row++)
+        {
+            for(star=1;star<=row;star++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
