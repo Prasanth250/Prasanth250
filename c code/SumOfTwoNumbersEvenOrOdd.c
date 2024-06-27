8f793f444e3f67442d84654ab2a6f04665adf3e7
Prasanth250 <147532770+Prasanth250@users.noreply.github.com>
2024-06-27 11:57:56 +0530

    Create SumOfTwoNumbersEvenOrOdd.c

---
+++ c code/SumOfTwoNumbersEvenOrOdd.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+int main() {
+   int a,b,sum;
+   printf("Enter the first number ");
+   scanf("%d",&a);
+   printf("Enter the second number ");
+   scanf("%d",&b);
+   sum=a+b;
+   printf("sum of a and b is %d",sum);
+   
+   if(sum%2==0)
+   printf("\nSum is Even");
+   else
+   printf("\nSum is Odd");
+    return 0;
+}
